e64e5292c4e789c04a7cc8361dd1f6d8a5f2a648
Anlly Castillo <110704559+AnllyAnalyCastilloVilleda@users.noreply.github.com>
2023-02-11 10:20:17 -0600

    Anlly
    Cambio prueba dos

--- pruebANALLY/src/pruebanally/PruebANALLY.java
+++ pruebANALLY/src/pruebanally/PruebANALLY.java
@@ -9,12 +9,13 @@ package pruebanally;
  * @author anlly
  */
 public class PruebANALLY {
-    
-    //Hola
 
-  }
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Mundo");
     }
     
 }
